d172638fae1104fd44d3a41be56e3e0bc53410e3
Fatih Kaya <1994274@gmail.com>
2016-06-01 13:36:58 +0000

    add length prefixed benchmark

--- perf/perf_vallist.c
+++ perf/perf_vallist.c
@@ -8,6 +8,8 @@ int main(int argc, char *argv[]) {
   int count = 1000000;
   int rc;
   int fixedval = 274;
+  int len = 6;
+  char *strval = "_jill_";
   struct jill_vallist vl;
 
   if (argc > 1)
@@ -23,6 +25,17 @@ int main(int argc, char *argv[]) {
     jill_vallist_term(&vl);
   }, {
     rc = jill_vallist_add_fixed (&vl, &fixedval);
+    assert (rc == 0);
+  });
+
+  jill_bench_run("length prefixed values", {
+    jill_vallist_init(&vl);
+    jill_vallist_set_grow(&vl, GROW);
+    jill_vallist_set_length_prefixed (&vl, 4);
+  }, {
+    jill_vallist_term(&vl);
+  }, {
+    rc = jill_vallist_add_length_prefixed (&vl, &len, strval, (size_t)len);
     assert (rc == 0);
   });
   //bench("every bit is off", 0);
