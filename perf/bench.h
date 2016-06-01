4864c0d63a21876c6c4a8497b939a5a027a14af9
Fatih Kaya <1994274@gmail.com>
2016-06-01 12:38:07 +0000

    add benchmark helper header

---
+++ perf/bench.h
@@ -0,0 +1,26 @@
+#ifndef JILL_BENCH_INCLUDED
+#define JILL_BENCH_INCLUDED
+
+#define jill_bench_declare(cnt) \
+  uint64_t jb_total; \
+  uint64_t jb_thr; \
+  struct jill_stopwatch jb_sw; \
+  int jb_count = cnt
+
+#define jill_bench_run(name, init_expr, term_expr, step_expr) do { \
+  init_expr\
+  jill_stopwatch_init (&jb_sw); \
+  for (int i = 0; i < jb_count; i++) { \
+    step_expr \
+  } \
+  jb_total = jill_stopwatch_term (&jb_sw); \
+  term_expr \
+  printf ("test: " name "\n"); \
+  jb_thr = (uint64_t) ((double) jb_count / (double) jb_total * 1000000); \
+  printf ("thr: %d ops/s\n", (int)jb_thr); \
+} while(0)
+
+#define jill_bench_thr() (jb_thr)
+#define jill_bench_set_count(cnt) jb_count = cnt
+
+#endif
--- perf/perf_jbitset.c
+++ perf/perf_jbitset.c
@@ -3,35 +3,38 @@
 #include <assert.h>
 #include "storage/jbitset.h"
 #include "utils/stopwatch.h"
+#include "bench.h"
 
 int main(int argc, char *argv[]) {
   int count = 1000000;
   int rc;
-  uint64_t total;
-  uint64_t thr;
-  struct jill_stopwatch sw;
   struct jill_bitset bs;
+
   if (argc > 1)
     count = atoi (argv[1]);
 
-  #define test(name, val_expr) do { \
+  jill_bench_declare(count);
+
+  #define init { \
     rc = jill_bitset_init (&bs); \
     assert (rc == 0); \
-    jill_stopwatch_init (&sw); \
-    for (int i = 0; i < count; i++) { \
-      rc = jill_bitset_add (&bs, val_expr); \
-      assert (rc == 0); \
-    } \
-    total = jill_stopwatch_term (&sw); \
-    jill_bitset_term (&bs); \
-    printf ("test: " name "\n"); \
-    thr = (uint64_t) ((double) count / (double) total * 1000000); \
-    printf ("thr: %d ops/s\n", (int)thr); \
-  } while(0)
+  }
 
-  test("every bit is set", 1);
-  test("every bit is off", 0);
-  test("rand() %% 5 == 0", rand() % 5 == 0);
+  #define term { \
+    jill_bitset_term (&bs); \
+  }
+
+  #define step(val_expr) { \
+    rc = jill_bitset_add (&bs, val_expr); \
+    assert (rc == 0); \
+  }
+
+  #define bench(name, val_expr) \
+    jill_bench_run(name, init, term, step(val_expr))
+
+  bench("every bit is set", 1);
+  bench("every bit is off", 0);
+  bench("rand() %% 5 == 0", rand() % 5 == 0);
 
   return 0;
 }
