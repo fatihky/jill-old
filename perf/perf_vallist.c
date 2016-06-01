540351940ffcbb71d3645c7f91d50acbd7de8876
Fatih Kaya <1994274@gmail.com>
2016-06-01 13:27:33 +0000

    remove unnecessary benchmark code

--- perf/perf_vallist.c
+++ perf/perf_vallist.c
@@ -4,30 +4,6 @@
 
 #define GROW 10
 
-#define init(type) { \
-  rc = jill_vallist_init (&bs); \
-  assert (rc == 0); \
-  jill_vallist_set_grow (&vl, GROW); \
-  rc = jill_vallist_set_val_type (&vl, type); \
-  assert (rc == 0); \
-}
-
-#define term { \
-  jill_vallist_term (&bs); \
-}
-
-#define step(step_expr) { \
-  rc = step_expr; \
-  assert (rc == 0); \
-}
-
-#define bench(name, type, val_expr) \
-  jill_bench_run(name, init(type), term, step(val_expr))
-
-void bench_fixed_size(int count) {
-  int rc;
-}
-
 int main(int argc, char *argv[]) {
   int count = 1000000;
   int rc;
