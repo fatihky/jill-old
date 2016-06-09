08b3609817ffb7bde2381493c8b22478516b3ffe
Fatih Kaya <1994274@gmail.com>
2016-06-09 22:51:09 +0000

    replace valindex with vallist.

--- src/core/vallist.c
+++ src/core/vallist.c
@@ -2,22 +2,22 @@
 #include <string.h>
 #include <assert.h>
 #include <errno.h>
-#include "valindex.h"
+#include "vallist.h"
 #include "../utils/cont.h"
 
-static struct jill_valindex_base **base_arr = NULL;
+static struct jill_vallist_base **base_arr = NULL;
 static int base_arr_len = 0;
 
-int jill_valindex_global_init() {
+int jill_vallist_global_init() {
   /*  register default types. */
   return 0;
 }
 
-int jill_valindex_last_internal_index() {
-  return 0; /*  no internal valindex implementations yet. */
+int jill_vallist_last_internal_index() {
+  return 0; /*  no internal vallist implementations yet. */
 }
 
-int jill_valindex_register (int type, struct jill_valindex_base *base) {
+int jill_vallist_register (int type, struct jill_vallist_base *base) {
   assert (type >= 0);
   assert (base);
   /*  check if index exceeds array's length */
@@ -33,31 +33,31 @@ int jill_valindex_register (int type, struct jill_valindex_base *base) {
 
   /*  base_arr_len is not incremented yet. so we can use it as array's
       index. */
-  ((struct jill_valindex_base **)ptr)[base_arr_len] = base;
+  ((struct jill_vallist_base **)ptr)[base_arr_len] = base;
   base_arr_len++;
   return 0;
 }
 
-struct jill_valindex *jill_valindex_create (int type, void *arg) {
+struct jill_vallist *jill_vallist_create (int type, void *arg) {
   assert (type >= 0);
   assert (type < base_arr_len);
   return base_arr[type]->create (arg);
 }
 
-int jill_valindex_insert (struct jill_valindex *self,
+int jill_vallist_insert (struct jill_vallist *self,
     struct jill_value *value) {
   return self->base->insert (self, value);
 }
 
-int jill_valindex_set (struct jill_valindex *self, int index,
+int jill_vallist_set (struct jill_vallist *self, int index,
     struct jill_value *value) {
   return self->base->set (self, index, value);
 }
 
-int jill_valindex_get (struct jill_valindex *self, struct jill_value **value) {
+int jill_vallist_get (struct jill_vallist *self, struct jill_value **value) {
   return self->base->get (self, value);
 }
 
-int jill_valindex_query (struct jill_valindex *self, void *query, void *result) {
+int jill_vallist_query (struct jill_vallist *self, void *query, void *result) {
   return self->base->query (self, query, result);
 }
--- src/core/vallist.h
+++ src/core/vallist.h
@@ -1,58 +1,58 @@
 #include "value.h"
 
-#ifndef JILL_VALINDEX_H_INCLUDED
-#define JILL_VALINDEX_H_INCLUDED
+#ifndef JILL_VALLIST_H_INCLUDED
+#define JILL_VALLIST_H_INCLUDED
 
-struct jill_valindex_base {
-  /*  valindex's type. unique */
+struct jill_vallist_base {
+  /*  vallist's type. unique */
   int type;
   /*  create funcion. argument can be anything. for example, it can be sub-type
       of the index. must return NULL on error and set errno. */
-  struct jill_valindex *(*create) (void *arg);
+  struct jill_vallist *(*create) (void *arg);
   /*  insert new element to the index. returns errno if error,
       zero on success. */
-  int (*insert) (struct jill_valindex *self, struct jill_value *value);
+  int (*insert) (struct jill_vallist *self, struct jill_value *value);
   /*  set value of the nth element. returns EINVAL if index is not valid. */
-  int (*set) (struct jill_valindex *self, int index,
+  int (*set) (struct jill_vallist *self, int index,
     struct jill_value *value);
   /*  get value of the nth element. */
-  int (*get) (struct jill_valindex *self, struct jill_value **value);
+  int (*get) (struct jill_vallist *self, struct jill_value **value);
   /*  query the index */
-  int (*query) (struct jill_valindex *self, void *query, void *result);
+  int (*query) (struct jill_vallist *self, void *query, void *result);
 };
 
-struct jill_valindex {
-  struct jill_valindex_base *base;
+struct jill_vallist {
+  struct jill_vallist_base *base;
   /*  custom fields can be placed here. */
 };
 
-/*  initialize valindex class globally. not thread-safe */
-int jill_valindex_global_init();
+/*  initialize vallist class globally. not thread-safe */
+int jill_vallist_global_init();
 
-/*  returns biggest internal struct jill_valindex type. */
-int jill_valindex_last_internal_index();
+/*  returns biggest internal struct jill_vallist type. */
+int jill_vallist_last_internal_index();
 
-/*  register new valindex type.
+/*  register new vallist type.
     some numbers are will be reserved(for example: from 0 to 4096) for jill's
     inernal use. and other numbers will not be restriced.
     returns 0 on success, errno on error. */
-int jill_valindex_register (int type, struct jill_valindex_base *base);
+int jill_vallist_register (int type, struct jill_vallist_base *base);
 
-/*  create new valindex with given type. */
-struct jill_valindex *jill_valindex_create (int type, void *arg);
+/*  create new vallist with given type. */
+struct jill_vallist *jill_vallist_create (int type, void *arg);
 
 /*  insert new element to the index. returns errno if error,
     zero on success. */
-int jill_valindex_insert (struct jill_valindex *self, struct jill_value *value);
+int jill_vallist_insert (struct jill_vallist *self, struct jill_value *value);
 
 /*  set value of the nth element. returns EINVAL if index is not valid. */
-int jill_valindex_set (struct jill_valindex *self, int index,
+int jill_vallist_set (struct jill_vallist *self, int index,
   struct jill_value *value);
 
 /*  get value of the nth element. */
-int jill_valindex_get (struct jill_valindex *self, struct jill_value **value);
+int jill_vallist_get (struct jill_vallist *self, struct jill_value **value);
 
 /*  query the index */
-int jill_valindex_query (struct jill_valindex *self, void *query, void *result);
+int jill_vallist_query (struct jill_vallist *self, void *query, void *result);
 
 #endif
