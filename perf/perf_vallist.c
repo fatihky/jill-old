440e9e360bb1364890d7683121ec59371dcfeda1
Fatih Kaya <1994274@gmail.com>
2016-06-19 09:46:19 +0000

    move length prefixed vallist performance test to new vallist api

--- perf/perf_vallist.c
+++ perf/perf_vallist.c
@@ -1,5 +1,6 @@
 #include "core/vallist.h"
 #include "vallist/fixed.h"
+#include "vallist/len_prefixed.h"
 #include "bench.h"
 
 #define GROW 10
@@ -27,7 +28,6 @@ int main(int argc, char *argv[]) {
     subtype = JILL_VALLIST_FIXED_I32;
     vl = jill_vallist_create (JILL_VALLIST_FIXED, &subtype);
     assert (vl);
-    //jill_vallist_set_grow(&vl, GROW);
     //jill_vallist_set_fixed (&vl, 4);
   }, {
     jill_vallist_destroy (vl);
@@ -36,31 +36,32 @@ int main(int argc, char *argv[]) {
     assert (rc == 0);
   });
 
-#if 0
   jill_bench_run("length prefixed values", {
-    jill_vallist_init(&vl);
-    jill_vallist_set_grow(&vl, GROW);
-    jill_vallist_set_length_prefixed (&vl, 4);
+    subtype = JILL_VALLIST_LENGTH_PREFIXED_I32;
+    vl = jill_vallist_create (JILL_VALLIST_LENGTH_PREFIXED, &subtype);
+    assert (vl);
+    val.valp = strval;
+    val.lenp = &len;
   }, {
-    jill_vallist_term(&vl);
+    jill_vallist_destroy (vl);
   }, {
-    rc = jill_vallist_add_length_prefixed (&vl, &len, strval, (size_t)len);
+    rc = jill_vallist_insert (vl, &val);
     assert (rc == 0);
   });
 
   jill_bench_run("length prefixed values - preallocate", {
-    jill_vallist_init(&vl);
-    jill_vallist_set_grow(&vl, GROW);
-    jill_vallist_set_length_prefixed (&vl, 4);
-    jill_vallist_lp_prealloc (&vl, len * count);
+    subtype = JILL_VALLIST_LENGTH_PREFIXED_I32;
+    vl = jill_vallist_create (JILL_VALLIST_LENGTH_PREFIXED, &subtype);
+    assert (vl);
+    val.valp = strval;
+    val.lenp = &len;
+    //jill_vallist_lp_prealloc (&vl, len * count);
   }, {
-    jill_vallist_term(&vl);
+    jill_vallist_destroy (vl);
   }, {
-    rc = jill_vallist_add_length_prefixed (&vl, &len, strval, (size_t)len);
+    rc = jill_vallist_insert (vl, &val);
     assert (rc == 0);
   });
-
-#endif
 
   return 0;
 }
